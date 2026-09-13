7d1b869aae3d9aa9e52ae83ec5232c7198983474
MUAZMMIL ALI <alimuzmmil294@gmail.com>
2026-09-13 16:48:21 +0500

    fix: remove duplicate entry in Hello component

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -11,6 +11,7 @@ const Hello = () => {
         <h1>I did voluntery in SMIT today!</h1>
         <h1>I did voluntery for the second time in SMIT today!</h1>
         <h1>I Completed my work in the EduHub Project</h1>
+        <h1>I Completed my work in the EduHub Project</h1>
         
       
     </div>
