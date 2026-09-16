38bb29eafd45f51d8fe7be211189ddfec84774a0
MUAZMMIL ALI <alimuzmmil294@gmail.com>
2026-09-16 19:27:21 +0500

    feat: add additional completion message for EduHub Project in Hello component

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -14,6 +14,7 @@ const Hello = () => {
         <h1>I Completed my work in the EduHub Project</h1>
         <h1>I Completed my work in the EduHub Project 4</h1>
         <h1>I Completed my work in the EduHub Project 5</h1>
+        <h1>I Completed my work in the EduHub Project 6</h1>
       
       
     </div>
